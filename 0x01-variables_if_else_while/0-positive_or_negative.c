3422049cb61f9c4e8c288f89d1cd8ee0b38d1ceb
Valency2009 <fakiyesivincent@gmail.com>
2022-12-11 12:07:52 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 
 /**
  * main - Entry point
- * Description - prints if random generated number is positive, zero or negative
+ * Description - prints if random generated number is +tive, zero or -tive
  * Return: Always 0 (Success)
 */
 int main(void)
